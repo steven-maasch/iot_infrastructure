c549cb4b0302326d4b4c02234b11b11abfd62af1
Steven Maasch <steven.maasch@gmx.de>
2015-11-26 21:03:45 +0100

    Require

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,15 +1,15 @@
 class iot_infrastructure::install inherits iot_infrastructure {
+    require iot_infrastructure::system_user
 
     $system_username = $::iot_infrastructure::system_username
     $system_usergroup = $::iot_infrastructure::system_usergroup
     $system_user_home = $::iot_infrastructure::system_user_home
     $dockerfiles_dir = $::iot_infrastructure::dockerfiles_dir
 
+
     contain 'git'
     contain 'docker'
     contain 'docker_compose'
-
-    contain 'iot_infrastructure::system_user'
 
     file { $system_user_home:
         ensure => directory,
